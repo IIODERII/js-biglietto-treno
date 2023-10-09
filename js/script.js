eec505a6faefd852ed3027545541e2b9b35dc098
IIODERII <oderrisi@gmail.com>
2023-10-09 15:17:50 +0200

    added elements to js file

--- js/script.js
+++ js/script.js
@@ -4,3 +4,32 @@
 // va applicato uno sconto del 20% per i minorenni
 // va applicato uno sconto del 40% per gli over 65.
 // L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
+
+const price = 0.21;
+
+let totPrice;
+
+let km = prompt("Inserisci il numero di chilometri che vuoi percorrere");
+
+if(isNaN(km)){
+    window.location.reload();
+}
+
+let age = prompt("Inserisci la tua età");
+
+if(isNaN(age)){
+    window.location.reload();
+}
+
+totPrice = price * km;
+
+if(age < 18){
+    totPrice = totPrice * 0.8;
+}
+
+if(age > 65){
+    totPrice = totPrice * 0.6;
+}
+
+document.getElementById('title').innerHTML = 'Il prezzo del suo biglietto è: €'+ totPrice.toFixed(2);
+
